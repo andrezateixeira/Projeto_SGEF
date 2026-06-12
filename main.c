b562282c59b58b6230c1b1f47a017db080d41f06
Pietra <pietramassarotti@gmail.com>
2026-06-12 17:00:23 -0300

    feat: adiciona menu principal para a navegação entre o sistema e os testes

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int sistemaGEF(void);
+int casosTestes(void);
 
 int main(int argc, char *argv[]) {
-	return 0;
+    int opcao;
+    
+    do {
+        printf("     BEM VINDO AO Sistema de Gerenciamento de Estoque e Faturamento\n");
+        printf("0 - Sair\n");
+        printf("1 - Acessar o sistema\n");
+        printf("2 - Acessar os testes\n");
+        printf("Opcao: ");
+        scanf("%d", &opcao);
+        
+        switch(opcao){
+            case 0:
+                printf("\nPrograma Finalizado!\n");
+                break;
+            case 1:
+                sistemaGEF();
+                break;
+            case 2:
+                casosTestes();
+                break;
+            default:
+                printf("Opcao invalida!\n");
+        }
+    } while(opcao != 0);
+    
+    return 0;
 }
+
