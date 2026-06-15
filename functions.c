47a5cc388fd7106950ac77320a8bfd9480e7a3fe
Pietra <pietramassarotti@gmail.com>
2026-06-15 17:40:29 -0300

    refactor: arruma funções para receberem os testes

--- functions.c
+++ functions.c
@@ -1,145 +1,154 @@
 #include <stdio.h>
 #include <string.h>
+
 #include "functions.h"
-#include "database.h"
 
-int i;
-
-int buscarIndiceProduto(int codigo) {
-    int i;
-    for (i = 0; i < db.qtdProdutos; i++) {
-        if (db.codigos[i] == codigo) {
-            return i; 
+int buscarIndiceProduto( Database *db, int codigo){
+	int i;
+    for(i = 0; i < db->qtdProdutos; i++)
+    {
+        if(db->produtos[i].codigo == codigo)
+        {
+            return i;
         }
     }
-    return -1; 
+    return -1;
 }
 
-char* validarCadastroProduto(
-    int codigo,
-    char nome[],
-    float preco,
-    int quantidade,
-    char categoria,
-    int codigos[],
-    int qtdProdutos
-) {
-    if (codigo < 0) {
+char* validarCadastroProduto( Database *db, Produto produto){
+    if(produto.codigo < 0)
+    {
         return "ERRO: codigo invalido";
     }
 
-    for (i = 0; i < qtdProdutos; i++) {
-        if (codigos[i] == codigo) {
-            return "ERRO: codigo ja cadastrado";
-        }
+    if(buscarIndiceProduto(db,produto.codigo) != -1)
+    {
+        return "ERRO: codigo ja cadastrado";
     }
 
-    if (strlen(nome) == 0) {
+    if(strlen(produto.nome) == 0)
+    {
         return "ERRO: nome invalido";
     }
 
-    if (preco <= 0) {
+    if(produto.preco <= 0)
+    {
         return "ERRO: preco invalido";
     }
 
-    if (quantidade < 0) {
+    if(produto.quantidade < 0)
+    {
         return "ERRO: quantidade invalida";
     }
 
-    if (categoria != 'A' && categoria != 'C' && categoria != 'E' && categoria != 'H' && categoria != 'M') {
+	if(produto.categoria != 'A' && produto.categoria != 'C' && produto.categoria != 'E' && produto.categoria != 'H' && produto.categoria != 'M')
+    {
         return "ERRO: categoria invalida";
     }
 
-	db.codigos[db.qtdProdutos] = codigo;
-    strcpy(db.nomes[db.qtdProdutos], nome);
-    db.precos[db.qtdProdutos] = preco;
-    db.quantidades[db.qtdProdutos] = quantidade;
-    db.categorias[db.qtdProdutos] = categoria;
-    db.qtdProdutos++;
+    if(db->qtdProdutos >= MAX_PRODUTOS)
+    {
+        return "ERRO: limite de produtos atingido";
+    }
+
+    db->produtos[db->qtdProdutos] = produto;
+    db->qtdProdutos++;
 
     return "Cadastro realizado com sucesso";
 }
 
-void mostrarProdutos() {
-    if(db.qtdProdutos == 0) {
+void mostrarProdutos(Database *db)
+{
+    if(db->qtdProdutos == 0)
+    {
         printf("Nenhum produto cadastrado.\n");
-    } else {
-        printf("\nPRODUTOS CADASTRADOS:\n");
-        for(i = 0; i < db.qtdProdutos; i++) {
-            printf("\nProduto %d\n", i + 1);
-            printf("Codigo: %d\n", db.codigos[i]);
-            printf("Nome: %s\n", db.nomes[i]);
-            printf("Preco: R$ %.2f\n", db.precos[i]);
-            printf("Quantidade: %d\n", db.quantidades[i]);
-            printf("Categoria: %c\n", db.categorias[i]);
-        }
+        return;
+    }
+
+	int i;
+    for(i = 0; i < db->qtdProdutos; i++)
+    {
+        Produto p = db->produtos[i];
+
+        printf("\nProduto %d\n", i + 1);
+        printf("Codigo: %d\n", p.codigo);
+        printf("Nome: %s\n", p.nome);
+        printf("Preco: %.2f\n", p.preco);
+        printf("Quantidade: %d\n", p.quantidade);
+        printf("Categoria: %c\n", p.categoria);
     }
 }
 
-char* excluirProduto(int codigo) {
-    int i;
-    int posicao = buscarIndiceProduto(codigo);
-    
-    if (posicao == -1) {
+char* excluirProduto(Database *db, int codigo){
+    int posicao = buscarIndiceProduto(db, codigo);
+
+    if(posicao == -1){
         return "ERRO: Produto nao encontrado";
     }
-    
-    for (i = posicao; i < db.qtdProdutos - 1; i++) {
-        db.codigos[i] = db.codigos[i + 1];
-        strcpy(db.nomes[i], db.nomes[i + 1]);
-        db.precos[i] = db.precos[i + 1];
-        db.quantidades[i] = db.quantidades[i + 1];
-        db.categorias[i] = db.categorias[i + 1];
+
+	int i;
+    for(i = posicao; i < db->qtdProdutos - 1; i++){
+        db->produtos[i] = db->produtos[i + 1];
     }
-    
-    db.qtdProdutos--;
+
+    db->qtdProdutos--;
     
     return "Produto excluido com sucesso";
 }
 
-char* validarCompra(int codigo, int quantidadeDesejada) {
-    int posicao = buscarIndiceProduto(codigo);
-    
-    if (posicao == -1) {
+char* validarCompra(Database *db, int codigo, int quantidadeDesejada){
+    int posicao = buscarIndiceProduto(db, codigo);
+
+    if(posicao == -1){
         return "ERRO: Produto nao encontrado";
     }
-    
-    if (db.quantidades[posicao] < quantidadeDesejada) {
+
+    if(db->produtos[posicao].quantidade< quantidadeDesejada){
         return "ERRO: Quantidade insuficiente em estoque";
     }
-    
-    db.quantidades[posicao] -= quantidadeDesejada;
-    
+
+    db->produtos[posicao].quantidade -=quantidadeDesejada;
+
     return "Compra realizada com sucesso";
 }
 
-char* entradaEstoque(int codigo, int quantidadeEntrada) {
-    if (quantidadeEntrada <= 0) {
+char* entradaEstoque(Database *db, int codigo, int quantidadeEntrada){
+    if(quantidadeEntrada <= 0){
         return "ERRO: Quantidade invalida para entrada";
     }
-    
-    int posicao = buscarIndiceProduto(codigo);
-    
-    if (posicao == -1) {
+
+    int posicao =
+        buscarIndiceProduto(
+            db,
+            codigo
+        );
+
+    if(posicao == -1)
+    {
         return "ERRO: Produto nao encontrado";
     }
-    
-    db.quantidades[posicao] += quantidadeEntrada;
-    
+
+    db->produtos[posicao].quantidade +=
+        quantidadeEntrada;
+
     return "Entrada realizada com sucesso";
 }
 
-void mostrarProdutosEstoqueBaixo(int limite) {
+void mostrarProdutosEstoqueBaixo(Database *db, int limite){
     int encontrou = 0;
-    printf("\n=== PRODUTOS COM ESTOQUE BAIXO (menor que %d) ===\n", limite);
-    for(i = 0; i < db.qtdProdutos; i++) {
-        if(db.quantidades[i] < limite) {
-            printf("Codigo: %d, Nome: %s, Quantidade: %d\n", 
-                   db.codigos[i], db.nomes[i], db.quantidades[i]);
+	int i;
+    for(i = 0; i < db->qtdProdutos; i++)
+    {
+        if(db->produtos[i].quantidade < limite){
+            printf("Codigo: %d Nome: %s Quantidade: %d\n", db->produtos[i].codigo, db->produtos[i].nome, db->produtos[i].quantidade);
+
             encontrou = 1;
         }
     }
-    if(encontrou != 1) {
+
+    if(!encontrou){
         printf("Nenhum produto com estoque baixo.\n");
     }
 }
+
+
